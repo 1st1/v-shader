651cb32a618be1bfb320e782b060600958d1def4
Yury Selivanov <yury@vercel.com>
2025-12-10 08:36:33 -0800

    Minimal state

--- app/env/env.tsx
+++ app/env/env.tsx
@@ -84,16 +84,16 @@ function colorToVec(hex: string) {
   return `vec3(${rgb[0].toFixed(3)}, ${rgb[1].toFixed(3)}, ${rgb[2].toFixed(3)})`;
 }
 
-function posToVec({x, y}: {x: number, y: number}) {
+function posToVec({ x, y }: { x: number, y: number }) {
   return `vec2(${(-x).toFixed(3)}, ${y.toFixed(3)})`
 }
 
-function dimToVec({x, y}: {x: number, y: number}) {
+function dimToVec({ x, y }: { x: number, y: number }) {
   return `vec2(${(x).toFixed(3)}, ${y.toFixed(3)})`
 }
 
-function Environment({lowPerf}: EnvironmentProps) {
-  const {gl} = useThree();
+function Environment({ lowPerf }: EnvironmentProps) {
+  const { gl } = useThree();
 
   const time = useRef(0);
   const clock = useRef(new Clock());
@@ -137,10 +137,10 @@ function Environment({lowPerf}: EnvironmentProps) {
     };
 
     window.removeEventListener('mousemove', onMove);
-    window.addEventListener('mousemove', onMove, {passive: true});
+    window.addEventListener('mousemove', onMove, { passive: true });
 
     window.removeEventListener('scroll', onScroll);
-    window.addEventListener('scroll', onScroll, {passive: true});
+    window.addEventListener('scroll', onScroll, { passive: true });
 
     const onResize = () => {
       const res = gl.getDrawingBufferSize(new Vector2());
@@ -154,7 +154,7 @@ function Environment({lowPerf}: EnvironmentProps) {
     };
 
     window.removeEventListener('resize', onResize);
-    window.addEventListener('resize', onResize, {passive: true});
+    window.addEventListener('resize', onResize, { passive: true });
 
     return () => {
       document.removeEventListener('mousemove', onMove);
@@ -165,72 +165,17 @@ function Environment({lowPerf}: EnvironmentProps) {
 
   const settings = readPaneSettings([
     'bg_scene',
-    'bg_pattern',
-    'bg_pattern_intensity',
-    'bg_pattern_scale',
-    'bg_visible',
-
-    'bg_blob1_color',
-    'bg_blob1_pos',
-    'bg_blob1_strength',
-    'bg_blob1_dims',
-    'bg_blob2_color',
-    'bg_blob2_pos',
-    'bg_blob2_strength',
-    'bg_blob2_dims',
-    'bg_blob3_color',
-    'bg_blob3_pos',
-    'bg_blob3_strength',
-    'bg_blob3_dims',
-    'bg_blob4_color',
-    'bg_blob4_pos',
-    'bg_blob4_strength',
-    'bg_blob4_dims',
-    'bg_blob_fade_after',
-    'bg_blob_fade_coeff',
-    'bg_blob_fade_max',
-    'bg_vignette_strength',
-
-    'fg_blob1_color',
-    'fg_blob1_pos',
-    'fg_blob1_strength',
-    'fg_blob1_dims',
-    'fg_blob2_color',
-    'fg_blob2_pos',
-    'fg_blob2_strength',
-    'fg_blob2_dims',
 
     'bg_aces',
     "bg_contrast",
     "bg_brightness",
     "bg_film_grain",
-
-    'ball_refract_bg',
-    'ball_refract_caustics',
-    'ball_light_color',
-    'ball_light_dims',
-    'ball_light_match_ar',
-    'ball_mat_color',
-    'ball_mat_light_mix',
-    'ball_speed',
-    'ball_speed_max_break_coeff',
-    'ball_refraction_index',
-    'ball_contrast_boost',
-    'ball_brightness_boost',
-    'ball_diffuseness',
-    'ball_reflect_intensity',
-    'ball_caustic_intensity',
-    'ball_bg_intensity',
-    'ball_bg_saturation',
-    'scene_max_dpr',
-
-    'debug_raymarch',
   ]);
 
   const uRad = new Float32Array([0.5, 0.28, 1, 0.21, 0.37]);
 
   function computePositions(uTime: number) {
-    uTime *= settings.ball_speed / 500.;
+    uTime *= 2. / 500.;
 
     const uPosX = new Float32Array(5);
     const uPosY = new Float32Array(5);
@@ -269,77 +214,10 @@ function Environment({lowPerf}: EnvironmentProps) {
       COLOR_SCENE: colorToVec(settings.bg_scene),
       SCENE_CONTRAST: settings.bg_contrast.toFixed(3),
       SCENE_BRIGHTNESS: settings.bg_brightness.toFixed(3),
-      BG_VISIBLE: settings.bg_visible ? 'true' : 'false',
-      BG_PATTERN: parseInt(settings.bg_pattern),
-      BG_PATTERN_INTENSITY: settings.bg_pattern_intensity.toFixed(2),
-      BG_PATTERN_SCALE: settings.bg_pattern_scale.toFixed(1),
-
-      BALL_LIGHT_COLOR: colorToVec(settings.ball_light_color),
-      BALL_LIGHT_DIMS: dimToVec(settings.ball_light_dims),
-      BALL_MATERIAL_COLOR: colorToVec(settings.ball_mat_color),
-      BALL_MAT_LIGHT_MIX: settings.ball_mat_light_mix.toFixed(3),
-      BALL_REFRACTION_INDEX: settings.ball_refraction_index.toFixed(3),
-      BALL_CONTRAST_BOOST: settings.ball_contrast_boost.toFixed(3),
-      BALL_BRIGHTNESS_BOOST: settings.ball_brightness_boost.toFixed(3),
-      BALL_DIFFUSENESS: settings.ball_diffuseness.toFixed(3),
-      BALL_REFLECT_INTENSITY: settings.ball_reflect_intensity.toFixed(3),
-      BALL_CAUSTIC_INTENSITY: settings.ball_caustic_intensity.toFixed(3),
-      BALL_BG_INTENSITY: settings.ball_bg_intensity.toFixed(3),
-      BALL_BG_SATURATION: settings.ball_bg_saturation.toFixed(3),
-
-      BG_BLOB1_COLOR: colorToVec(settings.bg_blob1_color),
-      BG_BLOB1_POS: posToVec(settings.bg_blob1_pos),
-      BG_BLOB1_STRENGTH: settings.bg_blob1_strength.toFixed(2),
-      BG_BLOB1_DIMS: dimToVec(settings.bg_blob1_dims),
-      BG_BLOB2_COLOR: colorToVec(settings.bg_blob2_color),
-      BG_BLOB2_POS: posToVec(settings.bg_blob2_pos),
-      BG_BLOB2_STRENGTH: settings.bg_blob2_strength.toFixed(2),
-      BG_BLOB2_DIMS: dimToVec(settings.bg_blob2_dims),
-      BG_BLOB3_COLOR: colorToVec(settings.bg_blob3_color),
-      BG_BLOB3_POS: posToVec(settings.bg_blob3_pos),
-      BG_BLOB3_STRENGTH: settings.bg_blob3_strength.toFixed(2),
-      BG_BLOB3_DIMS: dimToVec(settings.bg_blob3_dims),
-      BG_BLOB4_COLOR: colorToVec(settings.bg_blob4_color),
-      BG_BLOB4_POS: posToVec(settings.bg_blob4_pos),
-      BG_BLOB4_STRENGTH: settings.bg_blob4_strength.toFixed(2),
-      BG_BLOB4_DIMS: dimToVec(settings.bg_blob4_dims),
-
-      FG_BLOB1_COLOR: colorToVec(settings.fg_blob1_color),
-      FG_BLOB1_POS: posToVec(settings.fg_blob1_pos),
-      FG_BLOB1_STRENGTH: settings.fg_blob1_strength.toFixed(2),
-      FG_BLOB1_DIMS: dimToVec(settings.fg_blob1_dims),
-      FG_BLOB2_COLOR: colorToVec(settings.fg_blob2_color),
-      FG_BLOB2_POS: posToVec(settings.fg_blob2_pos),
-      FG_BLOB2_STRENGTH: settings.fg_blob2_strength.toFixed(2),
-      FG_BLOB2_DIMS: dimToVec(settings.fg_blob2_dims),
-
-      BLOB_FADE_AFTER: settings.bg_blob_fade_after.toFixed(2),
-      BLOB_FADE_COEFF: settings.bg_blob_fade_coeff.toFixed(2),
-      BLOB_MAX_FADE: settings.bg_blob_fade_max.toFixed(2),
     };
 
     if (settings.bg_aces) {
       defines.USE_ACES = true;
-    }
-
-    if (settings.bg_vignette_strength) {
-      defines.VIGNETTE = settings.bg_vignette_strength.toFixed(2);
-    }
-
-    if (settings.ball_refract_bg) {
-      defines.BALL_REFRACT_BG = true;
-    }
-
-    if (settings.ball_refract_caustics) {
-      defines.BALL_REFRACT_CAUSTICS = true;
-    }
-
-    if (settings.debug_raymarch) {
-      defines.DEBUG = true;
-    }
-
-    if (settings.ball_light_match_ar) {
-      defines.BALL_LIGHT_MATCH_AR = true;
     }
 
     if (lowPerf) {
@@ -384,7 +262,7 @@ function Environment({lowPerf}: EnvironmentProps) {
     lowPerf, ...Array.from(Object.values(settings)),
   ]);
 
-  useFrame(({gl}) => {
+  useFrame(({ gl }) => {
     if (!bgMaterial.current ||
       !bgCamera.current || !bgScene.current
     ) {
